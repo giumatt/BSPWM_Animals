9fc3c384e1e32af2c20e9d019ba0e839a8598f6e
giumatt <giumatt99@gmail.com>
2022-09-22 12:06:46 +0200

    Updates

--- scripts/screen_lock.sh
+++ scripts/screen_lock.sh
@@ -13,12 +13,12 @@ lock_Internal() {
     --ring-color=$fg --insidever-color=$verify --ringver-color=$verify \
     --insidewrong-color=$wrong --ringwrong-color=$wrong --line-uses-inside \
     --keyhl-color=$verify --separator-color=$verify --bshl-color=$verify \
-    --time-str="%H:%M" --time-size=100 --date-str="%a, %d %b" --date-size=45 \
+    --time-str="%H:%M" --time-size=100 --date-str="%a, %d %b" --date-size=35 \
     --verif-text="Verifying password..." --wrong-text="Wrong password!" --noinput-text="" \
-    --greeter-text="Type the password to unlock" --ind-pos="250:600" --time-font="SanFranciscoPro Display:style=Bold" \
+    --greeter-text="Type the password to unlock" --ind-pos="250:425" --time-font="SanFranciscoPro Display:style=Bold" \
     --date-font="SanFranciscoPro Display" --verif-font="SanFranciscoPro Display" --greeter-font="SanFranciscoPro Display" --wrong-font="SanFranciscoPro Display" \
-    --verif-size=23 --greeter-size=23 --wrong-size=23 --time-pos="250:400" --date-pos="250:500" \
-    --greeter-pos="250:700" --wrong-pos="250:725" --verif-pos="250:725" --date-color=$date \
+    --verif-size=20 --greeter-size=20 --wrong-size=20 --time-pos="250:300" --date-pos="250:350" \
+    --greeter-pos="250:500" --wrong-pos="250:525" --verif-pos="250:525" --date-color=$date \
     --time-color=$date --greeter-color=$fg --wrong-color=$wrong --verif-color=$verify \
     --pointer=default --refresh-rate=60 --pass-media-keys --pass-volume-keys
 }
